6afeaa25ed31adbd769d8525c8712263ce7dae5c
Shahzar Mazhar <mohdshahzar.1996@gmail.com>
2022-05-23 18:42:48 +0530

    Initial Commit

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import './scss/style.scss';
-import 'bootstrap';
-import { loadBody } from './pages/components/body';
-import { changeTab } from './modules/tabs-management';
-
-loadBody();
-changeTab('link-home');
--- src/modules/data.js
+++
@@ -1,8 +0,0 @@
-/*
-    This module imports data and images object, and export for further use
-*/
-
-import data from '../data/data.json';
-import images from './images';
-
-export {data, images};
